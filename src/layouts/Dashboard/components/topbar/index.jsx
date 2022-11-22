7637032330a386060fa86f5df9c4d9c314c45d62
Deepanshu Goel <symaticvisuals02@gmail.com>
2022-11-23 00:59:40 +0530

    user add ui done

--- src/layouts/Dashboard/components/topbar/index.jsx
+++ src/layouts/Dashboard/components/topbar/index.jsx
@@ -1,13 +1,155 @@
+import { Button, Label, Modal, TextInput } from 'flowbite-react';
 import React from 'react';
 
 function TopBar() {
+  const [isModalOpen, setIsModalOpen] = React.useState(false);
+  const onClose = () => {
+    setIsModalOpen(false);
+  };
+  const onOpen = () => {
+    setIsModalOpen(true);
+  };
+
+  const designations = [
+    'DIRECTOR',
+    'ASSOCIATE VICE PRESIDENT',
+    'ACCOUNT MANAGER',
+    'SENIOR ACCOUNT EXECUTIVE',
+    'Sr. Copywriter',
+    'Sr. Art Director',
+    'Sr. Account Executive'
+  ];
+  const serviceLines = ['Digital', 'Creative', 'Media', 'Analytics', 'PR'];
+  const [employee, setEmployee] = React.useState({});
+  // const createUser = async () => {};
+  const onChange = (e) => {
+    setEmployee({ ...employee, [e.target.name]: e.target.value });
+  };
   return (
     <div className="bg-white p-3 py-4">
       <div className="flex justify-between items-center">
         <h1 className="font-medium">Hi, Deepanshu Goel</h1>
-        <button className="bg-blue-700 text-white rounded-md px-4 py-2 hover:bg-blue-600">
-          Logout
-        </button>
+        <div className=" flex gap-3">
+          <button
+            onClick={onOpen}
+            className="inline-block rounded-lg px-4 py-1.5 tebuttont-base font-semibold leading-7 text-gray-900 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+            Add User +
+          </button>
+          <React.Fragment>
+            <Modal show={isModalOpen} size="2xl" popup={true} onClose={onClose}>
+              <Modal.Header />
+              <Modal.Body>
+                <div className="space-y-6 px-4 pb-4 sm:pb-6 lg:px-4 xl:pb-4">
+                  <h3 className="text-xl font-medium text-gray-900 dark:text-white">Add User</h3>
+                  <div className="flex gap-2">
+                    <div className="w-[30%]">
+                      <div className="mb-2 block">
+                        <Label value="Employee Code" />
+                      </div>
+                      <TextInput
+                        className=""
+                        name="empCode"
+                        placeholder="912312"
+                        type={'number'}
+                        onChange={onChange}
+                        required={true}
+                      />
+                    </div>
+                    <div className="flex-grow">
+                      <div className="mb-2 block">
+                        <Label value="Full Name" />
+                      </div>
+                      <TextInput
+                        className=""
+                        id="Name"
+                        type="text"
+                        name="name"
+                        onChange={onChange}
+                        placeholder="Jane Austin"
+                        required={true}
+                      />
+                    </div>
+                  </div>
+                  <div className="flex gap-2">
+                    <div className="flex flex-col w-[40%] ">
+                      <Label htmlFor="designation" value="Designation" />
+                      <select
+                        name="designation"
+                        id=""
+                        className="text-sm rounded-md mt-2"
+                        onChange={onChange}>
+                        {designations.map((designation, key) => (
+                          <option key={key} value={designation}>
+                            {designation}
+                          </option>
+                        ))}
+                      </select>
+                    </div>
+                    <div className=" flex-grow">
+                      <Label
+                        for="default-range"
+                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                        value="Annual CTC"
+                      />
+                      <TextInput
+                        className=""
+                        id="annualCTC"
+                        name="annualCtc"
+                        type="number"
+                        placeholder="2312312"
+                        required={true}
+                      />
+                    </div>
+                  </div>
+
+                  <div className="flex items-center gap-3">
+                    <div className="flex flex-col w-[40%] ">
+                      <Label htmlFor="designation" value="Designation" />
+                      <select name="" id="" className="text-sm rounded-md mt-2">
+                        {serviceLines.map((serviceLine, key) => (
+                          <option key={key} value={serviceLine}>
+                            {serviceLine}
+                          </option>
+                        ))}
+                      </select>
+                    </div>
+                    <div className="w-[30%]">
+                      <div className="mb-2 block">
+                        <Label htmlFor="name" value="Cost Per HR" />
+                      </div>
+                      <TextInput
+                        className=""
+                        id="costPerHr"
+                        placeholder="121"
+                        type={'number'}
+                        required={true}
+                      />
+                    </div>
+                    <div className="w-[30%]">
+                      <div className="mb-2 block">
+                        <Label htmlFor="name" value="HR Rate" />
+                      </div>
+                      <TextInput
+                        className=""
+                        id="hrRate"
+                        placeholder="1212"
+                        type={'number'}
+                        required={true}
+                      />
+                    </div>
+                  </div>
+
+                  <div className="w-full">
+                    <Button className="w-full">Create User</Button>
+                  </div>
+                </div>
+              </Modal.Body>
+            </Modal>
+          </React.Fragment>
+          <button className="bg-blue-700 text-white rounded-md px-4 py-2 hover:bg-blue-900">
+            Logout
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/pages/employees/index.jsx
+++ src/pages/employees/index.jsx
@@ -1,20 +1,82 @@
+import { Dropdown } from 'flowbite-react';
 import React from 'react';
-import EmployeesTable from './employee-table/employees-table';
+import { GetEmployees } from '../../hooks/get-employees-data';
+import TableInstance from './employee-table/table-instance';
 
 function Employees() {
+  const [tableData, setTableData] = React.useState(null);
+  const { data: employees, isLoading } = GetEmployees();
+  const [filtering, setFiltering] = React.useState([]);
+
+  React.useEffect(() => {
+    // const newTableData = employees?.data?.data?.rows.map((employee) => {
+    //   return {
+    //     ...employee,
+    //     daysLeft: Math.floor(
+    //       (new Date(employee.endDate) - new Date()) / (1000 * 60 * 60 * 24) + 2
+    //     ),
+    //   };
+    // });
+
+    let filters = [];
+    employees?.data.forEach((employee) => {
+      filters.push(employee.serviceLine);
+    });
+    filters = [...new Set(filters)];
+    setFiltering(filters);
+
+    setTableData(employees?.data);
+  }, [employees]);
+
+  if (isLoading) {
+    return (
+      <div className="flex w-full h-[50vh] justify-center items-center">
+        <h1 className="font-semibold">Loading...</h1>
+      </div>
+    );
+  }
+
+  if (tableData?.length === 0 || tableData === null || !tableData) {
+    return (
+      <div className="flex w-full h-[50vh] justify-center items-center">
+        <h1 className="font-semibold">No Records Found</h1>
+      </div>
+    );
+  }
+
   return (
     <div className="p-3 w-full">
       {/* Search Box Here */}
       <div className="flex flex-col flex-grow-0">
-        <div className=" overflow-x-auto">
+        <div className=" overflow-x-auto flex gap-4">
           <input
             type="text"
             placeholder="Search"
-            className="border-2 border-gray-300 bg-white w-full h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
+            className="border-2 border-gray-300 bg-white  h-auto px-5 pr-16 rounded-lg text-sm focus:outline-none flex-grow"
           />
+          <Dropdown label="Dropdown button" dismissOnClick={false}>
+            {filtering.map((filter, key) => (
+              <Dropdown.Item key={key}>
+                <div class="flex items-center">
+                  <input
+                    id={key}
+                    type="checkbox"
+                    onChange={() => {}}
+                    defaultChecked={true}
+                    class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                  />
+                  <label
+                    for={key}
+                    class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                    {filter}
+                  </label>
+                </div>
+              </Dropdown.Item>
+            ))}
+          </Dropdown>
         </div>
       </div>
-      <EmployeesTable />
+      <TableInstance tableData={tableData} />
     </div>
   );
 }
